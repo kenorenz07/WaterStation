714ca28554cbff790fd1ead7e8e2a46f3a83cba9
Keno Bacunawa <renzpalonpon@gmail.com>
2021-10-13 01:06:50 +0800

    add cors prefix

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'user/*', 'delivery_man/*'],
 
     'allowed_methods' => ['*'],
 
--- routes/user.php
+++ routes/user.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\User\AuthenticationController;
+use App\Http\Controllers\User\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,13 +17,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login',[AuthenticationController::class, 'login']);
+Route::post('/test',function(){
+    return "test";
+});
 
 Route::group( ['prefix' => '/v1','middleware' => ['auth:user-api','scopes:user'] ],function(){
     // authenticated staff routes here 
     Route::get('/details',[AuthenticationController::class, 'details']);
- 
-
     Route::post('logout',[AuthenticationController::class, 'logout']);
+
+    Route::get('/products/all',[ProductController::class,'index']);
 
 
 });
