9910164e7ce461ee2e12f61a98b25dd8ca3a6bbe
Keno Bacunawa <renzpalonpon@gmail.com>
2021-11-18 23:02:03 +0800

    custsjssjsj

--- resources/js/components/adminForms/Customer.vue
+++ resources/js/components/adminForms/Customer.vue
@@ -148,7 +148,7 @@
                 purok : '',
                 brgy : '',
                 city : '',
-                landmarl:'',
+                landmark:'',
                 additional_address : '',
                 image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F2180657%2Fadd_add_photo_upload_plus_icon&psig=AOvVaw2bCaC6AsrefFBHZ3Id8IAP&ust=1632066273765000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIC3-ejuiPMCFQAAAAAdAAAAABAD',
             }
