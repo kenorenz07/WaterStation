c81a1b2cb07b43ce829a7091a037dfddb2748e61
Keno Bacunawa <renzpalonpon@gmail.com>
2021-11-20 16:07:02 +0800

    delivery man update

--- app/Http/Controllers/DeliveryMan/AuthenticationController.php
+++ app/Http/Controllers/DeliveryMan/AuthenticationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\DeliveryMan;
 use App\Http\Controllers\Controller;
 use App\Models\DeliveryMan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AuthenticationController extends Controller
 {
@@ -13,6 +14,20 @@ class AuthenticationController extends Controller
         // $this->middleware('guest:delivery_man')->except('logout');
     }
 
+    public function uploadImage($image)
+    {
+        $exploded_base64 = explode(',', $image);
+        $decoded_base64  = base64_decode($exploded_base64[1]);
+        $extention = $this->string_between_two_string($exploded_base64[0], '/', ';');
+        
+        $fileName = time().'.'.$extention;
+        $path = storage_path('app/public/').$fileName;
+    
+        file_put_contents($path,$decoded_base64);   
+        
+        return $fileName;
+    }
+    
     public function login(Request $request)
     {
         $validator = $request->validate([
@@ -42,6 +57,32 @@ class AuthenticationController extends Controller
         $request->user()->token()->revoke();
 
         return "success";
+    }
+
+    public function update(Request $request)
+    {
+        $delivery_man = $request->user();
+        
+        $validated = $request->validate([
+            'name' => 'required',
+            'username' => 'required',
+            'phone_number' => 'required',
+        ]);
+
+        if(str_contains($request->image,'base64')){
+            if($delivery_man->image){
+                Storage::delete('app/public/updloads/'.$delivery_man->image);
+            }
+
+            $delivery_man->image =  $this->uploadImage($request->image);
+        }
+
+        $delivery_man->name = $request->name;
+        $delivery_man->username = $request->username;
+        $delivery_man->phone_number = $request->phone_number;
+        $delivery_man->password = $request->password ? bcrypt($request->password) : $delivery_man->password ;
+
+        return $delivery_man->save();
     }
 
 
